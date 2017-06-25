6f455b65e2f795995f78314966fb4f6e4404ec8e
dawish <742637254@qq.com>
2017-06-25 18:03:22 +0800

    #add dximageloader library

--- dximageloader/src/androidTest/java/com/dximageloader/ExampleInstrumentedTest.java
+++ dximageloader/src/androidTest/java/com/dximageloader/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package danxx.aoplib;
+package com.dximageloader;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 /**
- * Instrumentation test, which will execute on an Android device.
+ * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("danxx.aoplib.test", appContext.getPackageName());
+        assertEquals("com.dximageloader.test", appContext.getPackageName());
     }
 }
---
+++ dximageloader/src/main/java/com/dximageloader/DxImageLoader.java
@@ -0,0 +1,11 @@
+package com.dximageloader;
+
+/**
+ * Created by dawish on 2017/6/25.
+ */
+
+public class DxImageLoader {
+
+
+
+}

4fc539b0286a6678b3fcc9164400b449c504b737
dawish <742637254@qq.com>
2017-06-26 20:39:20 +0800

    #优化图片的存取和压缩

--- dximageloader/src/main/java/com/dximageloader/DxImageLoader.java
+++ dximageloader/src/main/java/com/dximageloader/DxImageLoader.java
@@ -4,13 +4,20 @@ import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Environment;
+import android.os.Looper;
+import android.util.Log;
 import android.util.LruCache;
 import android.widget.ImageView;
 
 import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.lang.ref.SoftReference;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -26,6 +33,7 @@ public class DxImageLoader {
      * Android开发之高效加载Bitmap http://www.cnblogs.com/absfree/p/5361167.html
      * Android线程同步 http://blog.csdn.net/peng6662001/article/details/7277851/
      */
+    private final static String TAG = "DxImageLoader";
     /**单例*/
     private static DxImageLoader mInstance;
     /**最大的文件缓存量*/
@@ -44,7 +52,7 @@ public class DxImageLoader {
             MAX_POOL_SIZE, KEEP_ALIVE, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
 
     /**内存缓存(软引用)*/
-    private LruCache<String, SoftReference<Bitmap>> mMemoryCache;
+    private LruCache<String, Bitmap> mMemoryCache;
     /**文件缓存*/
     private DiskLruCache mDiskLruCache;
 
@@ -67,10 +75,10 @@ public class DxImageLoader {
     public void init(Context context){
         int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
         int memoryCacheSize = maxMemory / 8;
-        mMemoryCache = new LruCache<String, SoftReference<Bitmap>>(memoryCacheSize){
+        mMemoryCache = new LruCache<String, Bitmap>(memoryCacheSize){
             @Override
-            protected int sizeOf(String key, SoftReference<Bitmap> value) {
-                return value.get().getByteCount() / 1024;// 获取图片占用运行内存大小
+            protected int sizeOf(String key, Bitmap value) {
+                return value.getByteCount() / 1024;// 获取图片占用运行内存大小
             }
         };
         File diskCacheDir = getDiskCacheDir(context,"dx_cache_images");
@@ -95,19 +103,84 @@ public class DxImageLoader {
      */
     private void addToMemoryCache(String key, Bitmap bitmap) {
         if (getFromMemoryCache(key) == null) {
-            mMemoryCache.put(key, new SoftReference<Bitmap>(bitmap));
+            mMemoryCache.put(key, bitmap);
         }
     }
 
     /**
-     *
+     * 根据key值获取内存中的bitmap
      * @param key
      * @return
      */
     private Bitmap getFromMemoryCache(String key) {
-        return mMemoryCache.get(key).get();
+        return mMemoryCache.get(key);
     }
 
+    /**
+     * 从文件缓存中获取图片
+     * @param url url（也是存取图片的key）
+     * @param dstWidth
+     * @param dstHeight
+     * @return
+     * @throws IOException
+     */
+    private Bitmap loadFromDisk(String url, int dstWidth, int dstHeight) throws IOException {
+        if (Looper.myLooper() == Looper.getMainLooper()) {
+            Log.w(TAG, "should not Bitmap in main thread");
+        }
+
+        if (mDiskLruCache == null) {
+            return null;
+        }
+        Bitmap bitmap = null;
+        String key = getKeyFromUrl(url);
+        DiskLruCache.Snapshot snapshot = mDiskLruCache.get(key);
+        if (snapshot != null) {
+            FileInputStream fileInputStream = (FileInputStream) snapshot.getInputStream(0);
+            FileDescriptor fileDescriptor = fileInputStream.getFD();
+            bitmap = decodeSampledBitmapFromFD(fileDescriptor, dstWidth, dstHeight);
+            if (bitmap != null) {
+                addToMemoryCache(key, bitmap);
+            }
+        }
+        return bitmap;
+    }
+
+    /**
+     * 压缩从文件取出的bitmap
+     * @param fd
+     * @param dstWidth
+     * @param dstHeight
+     * @return
+     */
+    private Bitmap decodeSampledBitmapFromFD(FileDescriptor fd, int dstWidth, int dstHeight) {
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFileDescriptor(fd, null, options);
+        options.inSampleSize = calInSampleSize(options, dstWidth, dstHeight);
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFileDescriptor(fd, null, options);
+    }
+    /**
+     * 根据给出的宽高计算图片的采样率
+     * @param options
+     * @param dstWidth
+     * @param dstHeight
+     * @return
+     */
+    private int calInSampleSize(BitmapFactory.Options options, int dstWidth, int dstHeight) {
+        //图片本来的宽高
+        int rawWidth = options.outWidth;
+        int rawHeight = options.outHeight;
+        int inSampleSize = 1;
+        //当图片本来的宽高 大于 需要的苦宽高 就减小采样率
+        if (rawWidth > dstWidth || rawHeight > dstHeight) {
+            float ratioWidth = (float) rawWidth / dstHeight;
+            float ratioHeight = (float) rawHeight / dstHeight;
+            inSampleSize = (int) Math.min(ratioWidth, ratioHeight);
+        }
+        return inSampleSize;
+    }
     /**
      * 获取设备的缓存路径
      * @param context
@@ -138,6 +211,37 @@ public class DxImageLoader {
             e.printStackTrace();
         }
         return 1;
+    }
+
+    /**
+     * 图片url作为存取的key做md5处理
+     * @param url
+     * @return
+     */
+    private String getKeyFromUrl(String url) {
+        String key;
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            messageDigest.update(url.getBytes());
+            byte[] m = messageDigest.digest();
+            return byteToString(m);
+        } catch (NoSuchAlgorithmException e) {
+            key = String.valueOf(url.hashCode());
+        }
+        return key;
+    }
+
+    /**
+     * byte To String
+     * @param b
+     * @return
+     */
+    private static String byteToString(byte[] b){
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < b.length; i ++){
+            sb.append(b[i]);
+        }
+        return sb.toString();
     }
     /**
      * step 1（必须步骤）
